70b26ae0738ecc34c0f63fba2866dd1e00ac0023
night-gold <braley.jo@gmail.com>
2019-01-15 23:55:55 +0100

    adding basic router configuration

--- main.go
+++ main.go
@@ -3,18 +3,30 @@ package main
 import (
 	"net/http"
 	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
-	message := r.URL.Path
+	vars := mux.Vars(r)
+	message := vars["message"]
 	message = strings.TrimPrefix(message, "/")
 	message = "Hello " + message
 	w.Write([]byte(message))
 }
 
 func main() {
-	http.HandleFunc("/", sayHello)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
-	}
+	//http.HandleFunc("/", sayHello)
+	r := newRouter()
+	http.ListenAndServe(":8080", r)
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	//	panic(err)
+	//}
+}
+
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", sayHello).Methods("GET")
+	r.HandleFunc("/{message}", sayHello).Methods("GET")
+	return r
 }
