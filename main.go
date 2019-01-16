f5d8c29b25bc988f50334ff4c1d2b258717d0c53
night-gold <braley.jo@gmail.com>
2019-01-16 22:47:46 +0100

    added response made from template

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 	"strings"
 
 	"github.com/gorilla/mux"
 )
+
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -27,6 +38,21 @@ func main() {
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/", sayHello).Methods("GET")
-	r.HandleFunc("/{message}", sayHello).Methods("GET")
+	r.HandleFunc("/hello/{message}", sayHello).Methods("GET")
+	r.HandleFunc("/modele", templating).Methods("GET")
 	return r
+}
+
+func templating(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+
+	data := TodoPageData{
+		PageTitle: "My TODO list",
+		Todos: []Todo{
+			{Title: "Task 1", Done: false},
+			{Title: "Task 2", Done: true},
+			{Title: "Task 3", Done: true},
+		},
+	}
+	tmpl.Execute(w, data)
 }
